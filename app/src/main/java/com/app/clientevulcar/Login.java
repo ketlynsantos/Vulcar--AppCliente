965cb2c9faf632fefb3d841f2a00f8f752ea1cdc
Ketlyn Milleny Castro dos Santos <Ketlyncastro2004@gmail.com>
2021-11-06 16:04:19 -0300

    Adding some variables

--- app/src/main/java/com/app/clientevulcar/Login.java
+++ app/src/main/java/com/app/clientevulcar/Login.java
@@ -1,10 +1,21 @@
 package com.app.clientevulcar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    public TextView txtRegister;
+    public TextView txtForgetPassword;
+    public EditText edtEmail;
+    public EditText edtPassword;
+    public AppCompatButton btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,6 +23,22 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         getSupportActionBar().hide();
+        getIds();
 
+        txtRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Login.this, Register.class));
+            }
+        });
+
+    }
+
+    public void getIds() {
+        txtRegister = this.findViewById(R.id.txt_register);
+        txtForgetPassword = this.findViewById(R.id.txt_forget_password);
+        edtEmail = this.findViewById(R.id.edt_email);
+        edtPassword = this.findViewById(R.id.edt_password);
+        btnLogin = this.findViewById(R.id.btn_login);
     }
 }
