55a81051aa82e679a4285ad1ef1f5593d4f1155a
Ketlyn Milleny Castro dos Santos <Ketlyncastro2004@gmail.com>
2021-11-06 15:46:43 -0300

    Registration screen completed

--- app/src/main/java/com/app/clientevulcar/Register.java
+++ app/src/main/java/com/app/clientevulcar/Register.java
@@ -4,12 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Login extends AppCompatActivity {
+public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_register);
 
         getSupportActionBar().hide();
 
