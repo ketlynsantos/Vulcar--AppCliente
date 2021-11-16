28362fd6154db41fc0a4de97f81f978adc69c88c
Ketlyn Milleny Castro dos Santos <Ketlyncastro2004@gmail.com>
2021-11-16 10:21:02 -0300

    Address screen ready

---
+++ app/src/main/java/com/app/clientevulcar/MyAddress.java
@@ -0,0 +1,35 @@
+package com.app.clientevulcar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import com.app.clientevulcar.R;
+
+public class MyAddress extends AppCompatActivity {
+
+    public ImageView imgBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_address);
+
+        getSupportActionBar().hide();
+        getIds();
+
+        imgBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MyAddress.this, Home.class));
+            }
+        });
+    }
+
+    private void getIds() {
+        imgBack = findViewById(R.id.img_back);
+    }
+}
--- app/src/main/java/com/app/clientevulcar/Register.java
+++ app/src/main/java/com/app/clientevulcar/Register.java
@@ -4,13 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
-import android.opengl.ETC1;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
 
