8ddb8ada167015d7ef5a5c26725fa1237cbf20e6
Ketlyn Milleny Castro dos Santos <Ketlyncastro2004@gmail.com>
2021-11-23 09:11:45 -0300

    Starting TabLayout

--- app/src/main/java/com/app/clientevulcar/Activitys/Search.java
+++ app/src/main/java/com/app/clientevulcar/Activitys/Search.java
@@ -1,58 +1,29 @@
 package com.app.clientevulcar.Activitys;
 
-import androidx.annotation.NonNull;
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.MenuItem;
-
-import com.app.clientevulcar.Activitys.Home;
-import com.app.clientevulcar.Activitys.Profile;
-import com.app.clientevulcar.Activitys.Requests;
+import android.view.View;
 import com.app.clientevulcar.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.app.clientevulcar.main.SectionsPagerAdapter;
 
 public class Search extends AppCompatActivity {
 
-    public BottomNavigationView bottomNavigationView;
-
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        ViewPager viewPager = findViewById(R.id.view_pager);
+        viewPager.setAdapter(sectionsPagerAdapter);
+        TabLayout tabs = findViewById(R.id.tabs);
+        tabs.setupWithViewPager(viewPager);
 
-        getIds();
-        bottomNavigationView.setSelectedItemId(R.id.search);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                switch (menuItem.getItemId()) {
-                    case R.id.search:
-                        return true;
-                    case R.id.home:
-                        startActivity(new Intent(getApplicationContext(), Home.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.requests:
-                        startActivity(new Intent(getApplicationContext(), Requests.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.profile:
-                        startActivity(new Intent(getApplicationContext(), Profile.class));
-                        overridePendingTransition(0, 0);
-                        return true;
-                }
-
-                return false;
-            }
-
-        });
-    }
-
-    private void getIds(){
-         bottomNavigationView = findViewById(R.id.bottom_navigation);
     }
 }
