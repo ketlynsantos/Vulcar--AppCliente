09015d91146acb72367b77d52a6f7c85a710d3c5
Ketlyn Milleny Castro dos Santos <Ketlyncastro2004@gmail.com>
2021-11-26 09:48:47 -0300

    Activity Profile completed

--- app/src/main/java/com/app/clientevulcar/Activitys/Requests.java
+++ app/src/main/java/com/app/clientevulcar/Activitys/Requests.java
@@ -2,17 +2,25 @@ package com.app.clientevulcar.Activitys;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.app.clientevulcar.R;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.koushikdutta.ion.builder.Builders;
 
 public class Requests extends AppCompatActivity {
 
     public BottomNavigationView bottomNavigationView;
+    public TextView txtServices;
+    public ImageView imgLogoBusiness;
+    public AppCompatButton btnAddBag;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,9 +54,19 @@ public class Requests extends AppCompatActivity {
             }
 
         });
+
+        btnAddBag.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     private void getIds(){
         bottomNavigationView = findViewById(R.id.bottom_navigation);
+        txtServices = findViewById(R.id.txt_services);
+        imgLogoBusiness = findViewById(R.id.img_logo);
+        btnAddBag = findViewById(R.id.btn_add_bag);
     }
 }
